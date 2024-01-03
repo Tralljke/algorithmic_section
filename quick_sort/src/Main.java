67d10c893260fa358ffd3179b1f24f5b12488d4f
Petr_Beliaev <pbelyaev@persona.ly>
2024-01-03 16:23:47 +0600

    quick sort section

---
+++ quick_sort/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> integers = List.of(10,10, 5, 2,2, 3);
+        System.out.println(quickSort(integers));
+    }
+
+    private static List<Integer> quickSort(List<Integer> inputData) {
+        if (inputData.size() < 2) {
+            return inputData;
+        }
+
+        Integer pivot = inputData.get(0);
+
+        List<Integer> less = new ArrayList<>();
+        List<Integer> more = new ArrayList<>();
+
+        for (int i = 1; i < inputData.size(); i++) {
+            if (inputData.get(i) < pivot) {
+                less.add(inputData.get(i));
+            } else {
+                more.add(inputData.get(i));
+            }
+        }
+
+        List<Integer> lessSorted = quickSort(less);
+        List<Integer> moreSorted = quickSort(more);
+
+        List<Integer> result = new ArrayList<>(lessSorted);
+        result.add(pivot);
+        result.addAll(moreSorted);
+
+        return result;
+    }
+}
